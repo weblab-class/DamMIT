ed4e431d13aa0c4c3f45c1184feb4050d89f0e55
Adem Bizid <bizid777@mit.edu>
2025-01-19 16:31:47 +0100

    Update profile.jsx

--- client/src/components/pages/profile.jsx
+++ client/src/components/pages/profile.jsx
@@ -1,5 +1,36 @@
+// App.jsx
+import React, { useState } from "react";
+import "./Profile.css";
+import ProfileCard from "../modules/profileCard";
+import "/Users/adembizid/work/skeleton/PauloHS-Silva-bizid777-volianyko/client/Adem.jpg";
+
 const Profile = () => {
-    return <div>Profile</div>;
+  const user = {
+    username: "_Adem.Bizid_",
+    major: "computer science",
+    dorm: "Next house",
+    classyear: "28'",
+    posts: 3,
+    completionRate: 24,
+    difficultyPoints: 1345,
+    percentRank: "2nd",
+    pointsRank: "3rd",
+    challenge: {
+      title: "Visit All Frats",
+      description:
+        "Yeah, I know we’ve all been too locked in the past few weeks. Let’s just chill for a bit and have some fun going to parties at ALL MIT frats!",
+      currentDifficulty: 2.43,
+      completed: 234,
+      likes: 617,
+      imageUrl: "Adem.jpg", // Placeholder image for profile picture
+    },
   };
-  
-  export default Profile;
+
+  return (
+    <div className="overall">
+      <ProfileCard {...user} />
+    </div>
+  );
+};
+
+export default Profile;
